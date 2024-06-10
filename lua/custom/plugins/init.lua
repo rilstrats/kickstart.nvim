598d3ddf5a7836a7cd6281d238679b6925e2f78f
Riley <91571914+rilstrats@users.noreply.github.com>
2024-06-10 15:56:34 -0600

    added plugins and language servers

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,7 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  { 'wakatime/vim-wakatime', lazy = false },
+  { 'xiyaowong/transparent.nvim', lazy = false },
+}
